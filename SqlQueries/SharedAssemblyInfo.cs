a2641a987ac20598f570cb956c7a975d7d49666e
Daan Jonkers <Daan.Jonkers@ing.nl>
2017-09-20 13:36:24 +0200

    Shared assemblie

---
+++ SqlQueries/SharedAssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("SRT2")]
+[assembly: AssemblyProduct("SqlQueries")]
+[assembly: AssemblyCopyright("Copyright © SRT2 2017")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
